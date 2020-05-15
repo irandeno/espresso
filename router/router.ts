ca3e676d3c8f46758b737a9009eb170e7cdbef3b
M. Bagher Abiat <zorofight94@gmail.com>
2020-05-15 14:13:10 +0430

    Regex routes => done

--- router/router.ts
+++ router/router.ts
@@ -1,36 +1,38 @@
-import { Route } from "./route.ts";
+import { Route } from './route.ts';
 type Handler = (ctx: any) => unknown;
-
+type Path = string | RegExp;
 export class Router {
-  
-  protected routes: Route[] = [];
+	protected routes: Route[] = [];
 
-  get(path: string, handler: Handler) {
-    this.add("GET", path, handler);
-    return this;
-  }
+	get(path: Path, handler: Handler) {
+		this.add('GET', path, handler);
+		return this;
+	}
 
-  post(path: string, handler: Handler) {
-    this.add("POST", path, handler);
-    return this;
-  }
+	post(path: Path, handler: Handler) {
+		this.add('POST', path, handler);
+		return this;
+	}
 
-  put(path: string, handler: Handler) {
-    this.add("PUT", path, handler);
-    return this;
-  }
+	put(path: Path, handler: Handler) {
+		this.add('PUT', path, handler);
+		return this;
+	}
 
-  delete(path: string, handler: Handler) {
-    this.add("DELETE", path, handler);
-    return this;
-  }
-  
-  patch(path: string, handler: Handler) {
-    this.add("PATCH", path, handler);
-    return this;
-  }
+	delete(path: Path, handler: Handler) {
+		this.add('DELETE', path, handler);
+		return this;
+	}
 
-  private add(method: string, path: string, handler: Handler) {
-    this.routes = [...this.routes, { method, path, handler }];
-  }
+	patch(path: Path, handler: Handler) {
+		this.add('PATCH', path, handler);
+		return this;
+	}
+
+	private add(method: string, path: Path, handler: Handler) {
+		this.routes = [
+			...this.routes,
+			{ method, path, handler }
+		];
+	}
 }
